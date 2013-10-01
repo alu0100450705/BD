9ee2f00af9201d28d5a10c4c3180c9774f174160
alu0100450705 <alu0100450705@ull.edu.es>
2013-10-01 12:40:25 +0100

    claves externas

---
+++ Database1/Galeria2.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table2]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Naccion] NUMERIC(3) NULL, 
+    [Ano] DATE NULL, 
+    [Regada] BIT NULL, 
+    [Regante] NVARCHAR(50) NULL, 
+    [Pagado] BIT NULL, 
+    [Pago_regante] NVARCHAR(50) NULL, 
+    CONSTRAINT [FK_Table2_ToTable] FOREIGN KEY ([Naccion]) REFERENCES [Table1]([Naccion]), 
+    CONSTRAINT [FK_Table2_ToTable_1] FOREIGN KEY ([Ano]) REFERENCES [Table1]([Ano])
+)
