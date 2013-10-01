e6610f7daa3a4daf406d527c19c8f98753ca58c9
alu0100450705 <alu0100450705@ull.edu.es>
2013-10-01 11:20:44 +0100

    prueba BD

---
+++ Database1/Galeria.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NCHAR(10) NOT NULL, 
+    [Apellido] NCHAR(10) NOT NULL, 
+    [Naccion] NUMERIC(3) NOT NULL, 
+    [Ano] DATE NOT NULL, 
+    [DNI] NCHAR(10) NULL, 
+    [Email] NCHAR(30) NULL, 
+    [Tlf] NUMERIC NULL, 
+    [comment] NTEXT NULL
+)
